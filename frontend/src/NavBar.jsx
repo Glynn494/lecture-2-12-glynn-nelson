b34199570acc1bc06a5388b0190da136e3228575
Glynn494 <glynnnelson176@gmail.com>
2026-03-10 14:49:30 -0400

    Initial commit

---
+++ frontend/src/NavBar.jsx
@@ -0,0 +1,16 @@
+import { Link } from 'react-router';
+
+function Navbar({ cartCount }) {
+    return (
+        <nav className="navbar">
+            <Link to="/">🏠 Home</Link>
+            <Link to="/inventory">📚 Books</Link>
+            <Link to="/magazines">📰 Magazines</Link>
+            <Link to="/cart">🛒 Cart ({cartCount})</Link>
+            <Link to="/add">➕ Add Book</Link>
+            <Link to="/add-magazine">➕ Add Magazine</Link>
+        </nav>
+    );
+}
+
+export default Navbar;
